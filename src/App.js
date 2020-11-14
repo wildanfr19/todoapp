68f04ae1199f2da6ff49821a4ae4859759e2ddad
fakhrulramadhan <fakhrulramadhansusanto@gmail.com>
2020-11-14 14:31:09 +0700

    Migrating HTML CSS To React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import './Styles.css';
+import './Homemadeapplefont.css';
+import './Bungeefont.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="frame">
+            <section className="header">
+                    <button className="header-btn main-black-color">Tambah</button>
+                    <h1 className="header-title">Todo list</h1>
+                   
+                    <button className="header-btn main-red-color">Hapus</button> 
+
+            </section>
+
+            <section classNameName="add">
+                <form className="add-form">
+                    <input type="text" className="add-input"/>
+                    <button className="add-btn main-black-color">Tambah</button>
+                </form>
+            </section>
+
+            <section className="todos">
+                <div className="todo">
+                    <span className="todo-text">Belajar React</span>
+                </div>
+                <div className="todo">
+                    <span className="todo-text">Belajar React</span>
+                </div>
+                <div className="todo">
+                    <span className="todo-text">Belajar React</span>
+                </div>
+                <div className="todo">
+                    <span className="todo-text">Belajar React</span>
+                </div>
+                <div className="todo">
+                    <span className="todo-text">Belajar React</span>
+                </div>
+                <div className="todo">
+                    <span className="todo-text">Belajar React</span>
+                </div>
+            </section>
+        </div>
     </div>
   );
 }
